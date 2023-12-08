8e49105f61e7824b186ba5b40dc949d4980ac05e
Mel <melaquansaffold@yahoo.com>
2023-12-07 20:55:46 -0500

    Landing page

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
   return (
     <div className="justify-center">
       <Navbar />
-      <h1 className="flex flex-wrap justify-center mt-8 mb-8">Landing </h1>
+      <h1 className="flex flex-wrap justify-center mt-8 mb-8">Landing Page </h1>
     </div>
   );
 };
