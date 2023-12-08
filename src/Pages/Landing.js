b1f4f4bcba10a08304850ad4dd983a19b8223dec
Mel <melaquansaffold@yahoo.com>
2023-12-07 22:15:17 -0500

    Created landing page

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Landing = () => {
   return (
     <div className="justify-center">
       <Navbar />
       <h1 className="flex flex-wrap justify-center mt-8 mb-8">Landing Page </h1>
+      <Footer />
     </div>
   );
 };
